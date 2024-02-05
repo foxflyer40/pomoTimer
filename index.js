3d377bd9570a652f86416a04c03278eec2877eb3
foxflyer40 <60403841+foxflyer40@users.noreply.github.com>
2024-02-05 12:42:04 -0500

    clean up code - add comments

--- index.js
+++ index.js
@@ -20,52 +20,51 @@ function timer() {
   resetTime.addEventListener("click", resetTimer);  
   
   function initializeTimer() {
+    // set interval for the timer to 1 second
     if (!intervalId) {
       intervalId = setInterval(startTimer, 1000)
     }
+    // set initial workTime in seconds
     if (workTime === 0) {
       workTime = workMinutes * 60;
-    }
-
-   
+    }   
   }
 
   function countDown() {
-
+    // calculate remainder after dividing workTime by 60 seconds
     seconds = workTime % 60;
+    // divide workTime by 60 and round down
     minutes = Math.floor(workTime / 60);
+    // decrement workTime by 1
     workTime--
   }
-
-  
+    
   function startTimer() {
-    // set interval for the timer to 1 second
-    // set initial workTime in seconds
-    // 
-    initializeTimer()
-    // console.log("start")
+    // check if interval is set and if not set to 1 second  This handles Start and Resume conditions
+    if (!intervalId) {
+      initializeTimer()
+    }
+    // run through calculate seconds and minutes
     countDown()
+    // handles 0-seconds condition to set to 2 digit format
     if (seconds < 10) {
       seconds = "0" + seconds
     }
-  
-    console.log(minutes);
-    console.log(seconds);
-
+    // Display mode
     modeEl.innerHTML = "Work"
+    // display time
     timerEl.innerHTML = `${minutes}: ${seconds}`;
-
   }
 
-
   function pauseTimer() {
-    console.log("pause")
+    // stop timer form counting down by removing interval
     intervalId = clearInterval(intervalId)
-    console.log(intervalId)
+    // Display mode
+    modeEl.innerHTML = "Paused"
   }
 
   function resumeTimer() {
-    console.log("resume")
+  // reset interval to 1 second to begin countdown again
     intervalId = setInterval(startTimer, 1000)
   }
 
